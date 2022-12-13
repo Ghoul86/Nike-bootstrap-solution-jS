e240a54210321a0e0c030dd7b893263a7292a7ad
apenteado <NomUtilisateurGit>
2022-12-13 14:52:45 +0100

    Nike JavaScript

---
+++ custom/JS/chaussures.js
@@ -0,0 +1,22 @@
+class Chaussures {
+    constructor (nom, prix, image){
+        this.nom = nom;
+        this.prix = prix;
+        this.image = image;
+        this.html = document.createElement("div");
+        this.html.className = "col-lg-4 col-md-6 col-12 d-flex align-items-center justify-content-center position-relative blockChaussure";
+        this.html.innerHTML = `<img src="images/${this.image}" width="100%" class="gray-scale">
+        <div class="position-absolute price d-flex align-items-center justify-content-center fw-bolder h5">${this.prix}€</div>
+        <div class="position-absolute model p-3  fw-bolder h3">${this.nom}</div>`
+    }
+    get getHtml (){
+        return this.html
+
+    } 
+};
+
+/* <div class="col-lg-4 col-md-6 col-12 d-flex align-items-center justify-content-center position-relative">
+<img src="images/jordan-1.webp" width="100%" class="gray-scale">
+<div class="position-absolute price d-flex align-items-center justify-content-center fw-bolder h5">26780 €</div>
+<div class="position-absolute model p-3  fw-bolder h3">Air Jordan 12</div>
+</div> */
